5c98fc87789c977244e4d63f54b4b2b66a20d693
Yestoya Minggus <yestoya.minggus@gmail.com>
2024-11-12 22:26:39 +0700

    scroll offset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50">
       <div
-        className="flex justify-between items-center max-w-6xl mx-auto
+        className="flex justify-between items-center max-w-7xl mx-auto
         md:my-2 bg-stone-950/30 p-4 md:rounded-xl backdrop-blur-lg"
       >
         <div className="text-white font-semibold text-lg uppercase">
